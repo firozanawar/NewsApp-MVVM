279dc21a643caef28dcc02e44b8c1813dbf5e784
firoz.anawar <firoz.anawar@timesinternet.in>
2020-06-27 23:02:47 +0530

    Search news fragment implemente with live data

--- app/src/main/java/com/firozanwar/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/firozanwar/newsapp/repository/NewsRepository.kt
@@ -6,7 +6,9 @@ import com.firozanwar.newsapp.db.ArticleDatabase
 class NewsRepository(val db: ArticleDatabase) {
 
     suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
-
         RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(queryString: String, pageNumber: Int) =
+        RetrofitInstance.api.searchNews(queryString, pageNumber)
 
 }
--- app/src/main/java/com/firozanwar/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/firozanwar/newsapp/ui/NewsViewModel.kt
@@ -16,6 +16,9 @@ class NewsViewModel(
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     val breakingNewsPage: Int = 1
 
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val searchNewsPage: Int = 1
+
     init {
         getBreakingNews("us")
     }
@@ -28,7 +31,26 @@ class NewsViewModel(
         breakingNews.postValue(handleBreakingNewsResponse(response))
     }
 
+    fun searchNews(queryString: String) = viewModelScope.launch {
+
+        // Show the loading
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(queryString, searchNewsPage)
+        searchNews.postValue(handleSearchNewsResponse(response))
+    }
+
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+
+        return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
 
         if (response.isSuccessful) {
             response.body()?.let { resultResponse ->
